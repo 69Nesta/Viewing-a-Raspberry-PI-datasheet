8dd543e724c5e49f57436f04c5170c5d466c366b
69Nesta <petitromeo74@gmail.com>
2024-05-05 16:56:35 +0200

    final

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
-import { composent, getParents } from "./lib";
+import { getParents } from "./lib";
 
 document.onmouseover = function (e) {
-    const data = composent[getParents(e.target).filter(element => composent[element])[0]]
+    const data = window.data[getParents(e.target).filter(element => window.data[element])[0]]
 
     document.getElementById('component_title').innerText = data ? data.title : '';
     document.getElementById('component_content').innerText = data ? data.content : '';
@@ -11,5 +11,19 @@ document.onmouseover = function (e) {
 const cursor = document.querySelector('.cursor');
 
 document.addEventListener('mousemove', function (e) {
-    cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 110%), 0)`
+    const box = document.getElementById('cursor')
+    const bowWidth = box.clientWidth
+    const bowHeight = box.clientHeight
+
+    let finalTranslateX = e.clientX
+    if ((e.clientX + 325) >= window.innerWidth)
+        finalTranslateX = window.innerWidth - 350
+    else if ((e.clientX - bowWidth / 2) < 1)
+        finalTranslateX = (bowWidth / 2) + 10
+
+    let finalTranslateY = e.clientY
+    if ((finalTranslateY - bowHeight - 30)<1)
+        finalTranslateY = 250
+
+    cursor.style.transform = `translate3d(calc(${finalTranslateX}px - 50%), calc(${finalTranslateY}px - 110%), 0)`
 });
